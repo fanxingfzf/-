aa697e500b7296891dcd00f7788b495e4fd6113f
MyClass-2 <3010006821@qq.com>
2022-07-10 10:15:21 +0800

    多模块创建

---
+++ model1/src/main/java/com/fzf/msp/MSPApplication.java
@@ -0,0 +1,15 @@
+package com.fzf.msp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fanZhiFei
+ * @create 2022/7/10
+ */
+@SpringBootApplication
+public class MSPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MSPApplication.class,args);
+    }
+}
